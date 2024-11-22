06c8e5d9d92e92c139fca04ce616845dad90e261
Loc <locnguyen110816@gmail.com>
2024-11-21 22:15:59 -0800

    Combine PreparingData.py with OutliersProcessing.py and some minor changes this file

--- PreparingData.py
+++ PreparingData.py
@@ -1,6 +1,46 @@
 import pandas as pd
-from OutliersProcessing import removeOutliers, getOutliers
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+
+# Helper function
+def findMinMax(data):
+  q1 = data.quantile(0.25)
+  q3 = data.quantile(0.75)
+  min = q1 - 1.5 * (q3 - q1)
+  max = q3 + 1.5 * (q3 - q1)
+  return min, max
+
+# Function to help us visualize the outliers. Produce boxplot and print out outliers
+def getOutliers(data, features):
+  for i, feature in enumerate(features, 1):
+    plt.subplot(1,len(features),i)
+    data[[feature]].boxplot()
+    
+    min, max = findMinMax(data[feature])
+    outliers_lower = data[feature] < min
+    outliers_upper = data[feature] > max
+    
+    if outliers_lower.any():
+      print(feature, "- Lower outliers:\n", data.loc[outliers_lower, feature])
+    if outliers_upper.any():
+      print(feature, "- Upper outliers:\n", data.loc[outliers_upper, feature])
+      
+  plt.show()
+  
+# Return a new set of data with outliers removed
+def removeOutliers(data, features):
+  removeIdx = pd.Series([False] * len(data))
+  for feature in features:
+    min, max = findMinMax(data[feature])
+    outliers_lower = data[feature] < min
+    outliers_upper = data[feature] > max
+    
+    removeIdx = removeIdx | outliers_lower | outliers_upper
+
+  return data.loc[~removeIdx]
+
+
+# Main
 
 data = pd.DataFrame(pd.read_csv('./SeoulBikeData.csv'))
 
@@ -21,7 +61,7 @@ data = pd.get_dummies(data, columns=['Seasons'], dtype=int)
 data = pd.get_dummies(data, columns=['Holiday'], dtype=int, drop_first=True)
 
 
-getOutliers(data, ['Rented Bike Count', 'Temperature', 'Humidity', 'Wind speed', 'Visibility', 'Solar Radiation', 'Rainfall', 'Snowfall'])
+# getOutliers(data, ['Rented Bike Count', 'Temperature', 'Humidity', 'Wind speed', 'Visibility', 'Solar Radiation', 'Rainfall', 'Snowfall'])
 
 # It seems like every rainy or snowy days are counted as outliers because the weather is normal most of the time.
 # Therefore, not going to remove outliers for Rainfall and Snowfall
@@ -32,22 +72,22 @@ getOutliers(data, ['Rented Bike Count', 'Temperature', 'Humidity', 'Wind speed',
 
 data = removeOutliers(data, ['Rented Bike Count', 'Temperature', 'Humidity', 'Wind speed', 'Visibility'])
 data = data.reset_index(drop=True)
-print(data)
-
 
 # Splitting data between categorical and numericals set for standardization
-categoricalFeatures = ['Hour', 'Seasons_Autumn', 'Seasons_Spring', 'Seasons_Summer', 'Seasons_Winter', 'Holiday_No Holiday']
-numericalFeatures = ['Rented Bike Count', 'Temperature', 'Humidity', 'Wind speed', 'Visibility', 'Solar Radiation', 'Rainfall', 'Snowfall']
-categoricalValues = data[categoricalFeatures]
-standardizedData = data.drop(columns=categoricalFeatures)
+categorical_features = ['Hour', 'Seasons_Autumn', 'Seasons_Spring', 'Seasons_Summer', 'Seasons_Winter', 'Holiday_No Holiday']
+numerical_features = ['Rented Bike Count', 'Temperature', 'Humidity', 'Wind speed', 'Visibility', 'Solar Radiation', 'Rainfall', 'Snowfall']
+categorical_values = data[categorical_features]
+numerical_values = data[numerical_features]
 
 scaler = StandardScaler()
-scaler.fit(standardizedData)
-standardizedData = scaler.transform(standardizedData)
-standardizedData = pd.DataFrame(standardizedData)
-standardizedData.columns = numericalFeatures
+numerical_values = scaler.fit_transform(numerical_values)
+numerical_values = pd.DataFrame(numerical_values)
+numerical_values.columns = numerical_features
 
-standardizedData = pd.concat([standardizedData, categoricalValues], axis=1)
-print(standardizedData)
+processed_data = pd.concat([numerical_values, categorical_values], axis=1)
+print(processed_data)
+
+# Save to a new CSV file
+processed_data.to_csv('ProcessedData.csv', index=False)
 
 
